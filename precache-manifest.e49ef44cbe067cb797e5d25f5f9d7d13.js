08bc689b5ed994d72f1e4cf1e405934c77a3049e
anisimovv <kirill.anisimovv@gmail.com>
2019-11-19 15:00:25 +0200

    Updates

---
+++ precache-manifest.e49ef44cbe067cb797e5d25f5f9d7d13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945c2c83209a226c4f8bff0b2b5949b2",
+    "url": "/anisimovv/headworks/index.html"
+  },
+  {
+    "revision": "9a37a979a0d2239a5215",
+    "url": "/anisimovv/headworks/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bd45ac791d722e81e9c0",
+    "url": "/anisimovv/headworks/static/js/2.3fb79c3e.chunk.js"
+  },
+  {
+    "revision": "9a37a979a0d2239a5215",
+    "url": "/anisimovv/headworks/static/js/main.999f3fd8.chunk.js"
+  },
+  {
+    "revision": "7e405c630eba82a5a47f",
+    "url": "/anisimovv/headworks/static/js/runtime-main.19468167.js"
+  }
+]);
